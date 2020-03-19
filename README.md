f981772cfa7df5a14f17ea55e7149a42c3bdad3b
Sagar Mohan <sagarmohan846@gmail.com>
2020-03-19 13:24:15 +0530

    Update Script, Readme and Delete RAWs
    Formatted the script to follow the PEP 8 style guide.
    Update README to remove private drive link.
    Delete Certificate RAW Image and Name List.

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # HackBout E-Certificate Generator 
 
 An intelligible Python script just to reduce the manual labour of creating customised e-certificates for all the Hacker's who bout on innovative grounds for 36 hours at Hack Club NMIT First Hackathon [HackBout](https://www.hackbout.tech). 
-All the HackBout finalists can download their  E-Certificates from here:
-[Hacker's Certificates Drive Link](https://drive.google.com/drive/folders/1yZLgV5zlwrsg_y5nA_AWxqvecN4Id32x?usp=sharing)
+All the HackBout finalists can download their  E-Certificates through the emails sent to them.
 
 
-## Libraries Used:
+## Requirements:
 
-- xlrd
-- re
-- Pillow (PIL fork by Alex Clark)
+- [xlrd](https://github.com/python-excel/xlrd)
+- [re(gex)](https://docs.python.org/3/howto/regex.html)
+- [Pillow (PIL fork by Alex Clark)](https://github.com/python-pillow/Pillow)
+- Name list file
+- Blank Certificate Image
+
+## Usage:
+
+``` 
+python script.py
+```
+
--- script.py
+++ script.py
@@ -1,52 +1,55 @@
+import re
 import xlrd
-
 from xlrd import open_workbook
+from PIL import Image, ImageDraw, ImageFont
 
+# Open the excel file for reading names in first column
 book = xlrd.open_workbook("book1.xlsx")
 sheet = book.sheet_by_index(0)
 
 original_list = []
 clean_list = []
 
-for k in range(1,sheet.nrows):
+for k in range(1, sheet.nrows):
     original_list.append(str(sheet.row_values(k)))
 
 print(original_list)
 
 
-#Removing Special Character from the Data using Regex
-import re
-#cleanString = re.sub('\W+'," ", "['Neel Kamath']" )
+# Removing Special Character from the Data using Regex
+# CleanString = re.sub('\W+'," ", "['Neel Kamath']" )
 for i in original_list:
     clean_string = re.sub('[^A-Za-z0-9]+', " ", i)
-    clean_list.insert( 0 , clean_string)
-    
-print (clean_list) 
+    clean_list.insert(0, clean_string)
 
 
-from PIL import Image, ImageDraw, ImageFont
-  
+print(clean_list)
+
+
 def hacker_name(name):
-    image = Image.open('certificate_raw.jpg')   # create Image object with the input image
-    draw = ImageDraw.Draw(image)    # initialise the drawing context with the image object as background
+    # create Image object with the input image
+    image = Image.open('certificate_raw.jpg')
+    # initialise the drawing context with the image object as background
+    draw = ImageDraw.Draw(image)
     font = ImageFont.truetype('Roboto-Bold.ttf', size=100)
 
     if len(j) < 12:
-        (x , y) = (2090, 1520)
+        (x, y) = (2090, 1520)
 
     elif len(j) < 20:
-        (x , y) = (1950, 1520)
-            
+        (x, y) = (1950, 1520)
+
     else:
-        (x , y) = (1850, 1520)
-                 
-    color = 'rgb(0, 0, 0)' # black color
+        (x, y) = (1850, 1520)
+
+    color = 'rgb(0, 0, 0)'  # black color
     draw.text((x, y), name, fill=color, font=font)
     image.save(name + ".jpg")  # save the edited image
 
     #image.save('optimized.png', optimize=True, quality=20)
 
+
 for j in clean_list:
     hacker_name(j)
     print(len(j))
-    print (j)
+    print(j)
